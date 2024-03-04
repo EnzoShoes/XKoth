fa9de816c8a63126167aff0a49919d543d5c91fe
marvin2k0 <marvin@leiers.org>
2024-03-04 21:50:35 +0100

    Add AbstractCommandLoader.java

--- src/main/java/me/enzosocks/xkoth_socks/commands/CommandHandler.java
+++ src/main/java/me/enzosocks/xkoth_socks/commands/CommandHandler.java
@@ -1,66 +1,24 @@
 package me.enzosocks.xkoth_socks.commands;
 
-import me.enzosocks.xkoth_socks.Loader;
 import me.enzosocks.xkoth_socks.XKoth;
 import me.enzosocks.xkoth_socks.commands.koth.*;
-import me.enzosocks.xkoth_socks.utils.messages.MessageUtil;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class CommandHandler implements Loader, CommandExecutor {
-	private List<SubCommand> commands = new ArrayList<>();
-	private final String LABEL = "koth";
-	private final List<String> ALIASES = Arrays.asList("xkoth");
-	private final SubCommand defaultCommand;
-	private XKoth plugin;
-
+public class CommandHandler extends AbstractCommandLoader {
 	public CommandHandler(XKoth plugin) {
-		this.plugin = plugin;
-		defaultCommand = new KothHelpCommand(plugin, commands);
+		super(plugin, "koth");
 
-		commands.add(new KothStartCommand(plugin));
-		commands.add(new KothStopCommand(plugin));
-		commands.add(new KothCancelCommand(plugin));
-		commands.add(new KothListCommand(plugin));
-		commands.add(new KothInfoCommand(plugin));
-		commands.add(new KothVersionCommand(plugin));
-		commands.add(new KothCreateCommand(plugin));
-		commands.add(new KothDeleteCommand(plugin));
-		commands.add(new KothTimeCommand());
-		commands.add(defaultCommand);
-	}
+		addSubCommand(new KothStartCommand(plugin));
+		addSubCommand(new KothStopCommand(plugin));
+		addSubCommand(new KothCancelCommand(plugin));
+		addSubCommand(new KothListCommand(plugin));
+		addSubCommand(new KothInfoCommand(plugin));
+		addSubCommand(new KothVersionCommand(plugin));
+		addSubCommand(new KothCreateCommand(plugin));
+		addSubCommand(new KothDeleteCommand(plugin));
+		addSubCommand(new KothTimeCommand());
 
-	@Override
-	public void load() {
-		plugin.getCommand(LABEL).setExecutor(this);
-	}
-
-	@Override
-	public void unload() {
-		plugin.getCommand(LABEL).setExecutor(null);
-	}
-
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (!label.equalsIgnoreCase(LABEL) && ALIASES.stream().noneMatch(s -> s.equalsIgnoreCase(label))) {
-			return true;
-		}
-
-		if (args.length == 0)
-			return defaultCommand.execute(sender, args);
-
-		for (SubCommand subCommand : commands) {
-			if (subCommand.getLabel().equalsIgnoreCase(args[0])) {
-				return subCommand.execute(sender, args);
-			}
-		}
-
-		MessageUtil.sendMessage(sender, "&cInvalid usage. Use /koth help for more info.");
-		return true;
+		final SubCommand helpCommand = new KothHelpCommand(plugin, getSubcommands());
+		setDefaultCommand(helpCommand);
+		addSubCommand(helpCommand);
 	}
 }
